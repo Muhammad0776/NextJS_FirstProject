19ba4944fb55cac15d334fa8095eaa5d7d97eced
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-08-17 11:48:03 +0500

    redux, pages, container...

--- pages/404.js
+++ pages/404.js
@@ -4,10 +4,11 @@ const Page404 = () => {
     const [margin, setmargin] = useState(100)
 
     return (
-        <div style={{
+        <div className="d-flex justify-content-center align-items-center" style={{
             margin: `${margin}px`
         }}>
-            <h1>404</h1>
+            <div className="border-end px-4 text-danger"><h1>404</h1></div>
+            <p className="d-flex justify-content-center align-items-center ms-4 text-danger">This page could not be found</p>
         </div>
     )
 }
---
+++ redux/types.js
@@ -0,0 +1,3 @@
+export const SET_TODOS = "SET_TODOS";
+
+export const TOGGLE_MENU = "TOGGLE_MENU";
